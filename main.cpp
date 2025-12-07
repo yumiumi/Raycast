502ec8929fbea12909edc59b3077298e574e9792
Yumi <galkina1611@gmail.com>
2025-12-07 01:22:14 +0100

    feat: add simple map editor

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ const int scr_height = 720;
 const int scr_width = 1280;
 const int map_height = 12;
 const int map_width = 24;
+int tile_size = 16;
+
+bool map_editor = false; 
 
 enum TileType {
     EMPTY,
@@ -126,6 +129,17 @@ Color get_lighting(Color wall, int side) {
     return srgb_lighting;
 }
 
+Vector2 convert_to_px(Vector2 v) {
+	Vector2 v_px = { v.x * tile_size, v.y * tile_size };
+	//centerize
+	float w = scr_width/2 - map_width/2 * tile_size;
+	float h = scr_height/2 - map_height/2 * tile_size;
+	return { v_px.x + w , v_px.y + h };
+}
+
+void edit_map() {
+}
+
 int main() {
     InitWindow(scr_width, scr_height, "Raycast");
 
@@ -139,122 +153,161 @@ int main() {
     SetTargetFPS(60);
     rlImGuiSetup(true);
     while (!WindowShouldClose()) {
-
-        // raycasting loop
-        for (int x = 0; x <= scr_width; x++) {
-            float camera_x = 2 * x / float(scr_width) - 1; // x-coordinate in camera space (value from -1 to 1)
-            player_dir = Vector2Normalize(player_dir);
-            camera_plane = Vector2Normalize(camera_plane);
-
-            // half length of camera_plane from given angle
-            float fov = std::tan(DEG2RAD * (66.f / 2.f));
-
-            // calculate ray direction
-            ray_dir.x = player_dir.x + camera_plane.x * fov * camera_x;
-            ray_dir.y = player_dir.y + camera_plane.y * fov * camera_x;
-            assert(ray_dir.x != 0.f || ray_dir.y != 0.f);
-            ray_dir = Vector2Normalize(ray_dir);
-
-            // length of ray from one x or y-side to next x or y-side
-            Vector2 delta_dist;
-            // calculate delta distance
-            delta_dist.x = std::abs(1.f / ray_dir.x);
-            delta_dist.y = std::abs(1.f / ray_dir.y);
-
-            int hit = 0; // was wall hit?
-            int side; // which side of wall NS or WE
-
-            // calculate what direction to step in x or y-direction (either +1 or -1)
-            int step_x = (ray_dir.x < 0) ? -1 : 1;
-            int step_y = (ray_dir.y < 0) ? -1 : 1;
-
-            // current cell we are in
-            int map_x = int(player_pos.x);
-            int map_y = int(player_pos.y);
-
-            // distance from current position to next x or y-side
-            Vector2 side_dist;
-            Vector2 fract = Vector2{ player_pos.x - map_x, player_pos.y - map_y };
-            side_dist.x = ray_dir.x < 0 ? fract.x : 1.f - fract.x;
-            side_dist.y = ray_dir.y < 0 ? fract.y : 1.f - fract.y;
-            side_dist = Vector2Multiply(side_dist, delta_dist);
-
-            // perform DDA
-            while (hit == 0) {
-                if (side_dist.x < side_dist.y) {
-                    side_dist.x += delta_dist.x;
-                    map_x += step_x;
-                    side = 0;
-                }
-                else {
-                    side_dist.y += delta_dist.y;
-                    map_y += step_y;
-                    side = 1;
-                }
-                // check if ray has hit a wall
-                if (map[map_y][map_x] != EMPTY) {
-                    hit = 1;
+        
+        if (IsKeyPressed(KEY_TAB)) {
+            map_editor = !map_editor;
+        }
+        if (map_editor) {
+            for (int y = 0; y < map_height; y++) {
+                for (int x = 0; x < map_width; x++) {
+                    Vector2 pos = { x, y };
+                    Vector2 tile_sz = { tile_size, tile_size };
+                    if (map[y][x] == EMPTY) {
+                        DrawRectangleV(convert_to_px(pos), tile_sz, { 40, 40, 40, 255 });
+                    }
+                    else if (map[y][x] == GRAY_WALL) {
+                        DrawRectangleV(convert_to_px(pos), tile_sz, GRAY);
+                    }
+                    else if (map[y][x] == BLUE_WALL) {
+                        DrawRectangleV(convert_to_px(pos), tile_sz, BLUE);
+                    }
+                    else if (map[y][x] == RED_WALL) {
+                        DrawRectangleV(convert_to_px(pos), tile_sz, RED);
+                    }
+                    else if (map[y][x] == GREEN_WALL) {
+                        DrawRectangleV(convert_to_px(pos), tile_sz, GREEN);
+                    }
                 }
             }
-
-            float ray_len = (side == 0) ? side_dist.x - delta_dist.x : side_dist.y - delta_dist.y;
-            Vector2 ray = Vector2Scale(ray_dir, ray_len);
-            // perpendicular distance from wall to camera plane
-            float perp_wall_dist = Vector2DotProduct(player_dir, ray);
-
-            // calculate height of line to render
-            int line_height = int(scr_height / perp_wall_dist);
-
-            // calculate the lowest and highest pixel to fill in current stripe
-            int draw_start = - line_height / 2 + scr_height / 2;
-            if (draw_start < 0) {
-                draw_start = 0;
-            }
-            int draw_end = line_height / 2 + scr_height / 2;
-            if (draw_end >= scr_height) {
-                draw_end = scr_height - 1;
-            }
-
-            // render walls
-            DrawLine(x, draw_start, x, draw_end, get_lighting(w_color[map[map_y][map_x]], side));
-            // render floor
-            DrawLine(x, draw_end, x, scr_height, get_lighting({ 100,100,100,255 }, 1));
-        }
-
-        double delta_time = GetFrameTime();
-        float move_speed = delta_time * 5.f;
-        float rot_speed = delta_time * 2.f;
-        // movement
-        if (IsKeyDown(KEY_W)) {
-           Vector2 velocity = Vector2Scale(player_dir, move_speed);
-            if(map[int(player_pos.y)][int(player_pos.x + velocity.x)] == EMPTY) {
-                player_pos.x += velocity.x;
-            }
-            if (map[int(player_pos.y + velocity.y)][int(player_pos.x)] == EMPTY) {
-                player_pos.y += velocity.y;
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                float w = scr_width/2 - map_width/2 * tile_size;
+                float h = scr_height/2 - map_height/2 * tile_size;
+                Vector2 mouse_px = GetMousePosition();
+                mouse_px = { mouse_px.x - w, mouse_px.y - h };
+                // Convert mose position from pixels to tiles
+                Vector2 mouse_tile = { floor(mouse_px.x / tile_size), floor(mouse_px.y / tile_size) };
+                map[int(mouse_tile.y)][int(mouse_tile.x)] = GRAY_WALL;
             }
         }
-        if (IsKeyDown(KEY_S)) {
-            Vector2 velocity = Vector2Scale(player_dir, move_speed);
-            if (map[int(player_pos.y)][int(player_pos.x - velocity.x)] == EMPTY) {
-                player_pos.x -= velocity.x;
-            }
-            if (map[int(player_pos.y - velocity.y)][int(player_pos.x)] == EMPTY) {
-                player_pos.y -= velocity.y;
-            }
-        }
+        else {
+            // raycasting loop
+            for (int x = 0; x <= scr_width; x++) {
+                float camera_x = 2 * x / float(scr_width) - 1; // x-coordinate in camera space (value from -1 to 1)
+                player_dir = Vector2Normalize(player_dir);
+                camera_plane = Vector2Normalize(camera_plane);
 
-        if (IsKeyDown(KEY_A)) {
-            player_dir = Vector2Rotate(player_dir, rot_speed);
-            camera_plane = Vector2Rotate(camera_plane, rot_speed);
-        }
-        if (IsKeyDown(KEY_D)) {
-            player_dir = Vector2Rotate(player_dir, -rot_speed);
-            camera_plane = Vector2Rotate(camera_plane, -rot_speed);
+                // half length of camera_plane from given angle
+                float fov = std::tan(DEG2RAD * (66.f / 2.f));
+
+                // calculate ray direction
+                ray_dir.x = player_dir.x + camera_plane.x * fov * camera_x;
+                ray_dir.y = player_dir.y + camera_plane.y * fov * camera_x;
+                assert(ray_dir.x != 0.f || ray_dir.y != 0.f);
+                ray_dir = Vector2Normalize(ray_dir);
+
+                // length of ray from one x or y-side to next x or y-side
+                Vector2 delta_dist;
+                // calculate delta distance
+                delta_dist.x = std::abs(1.f / ray_dir.x);
+                delta_dist.y = std::abs(1.f / ray_dir.y);
+
+                int hit = 0; // was wall hit?
+                int side; // which side of wall NS or WE
+
+                // calculate what direction to step in x or y-direction (either +1 or -1)
+                int step_x = (ray_dir.x < 0) ? -1 : 1;
+                int step_y = (ray_dir.y < 0) ? -1 : 1;
+
+                // current cell we are in
+                int map_x = int(player_pos.x);
+                int map_y = int(player_pos.y);
+
+                // distance from current position to next x or y-side
+                Vector2 side_dist;
+                Vector2 fract = Vector2{ player_pos.x - map_x, player_pos.y - map_y };
+                side_dist.x = ray_dir.x < 0 ? fract.x : 1.f - fract.x;
+                side_dist.y = ray_dir.y < 0 ? fract.y : 1.f - fract.y;
+                side_dist = Vector2Multiply(side_dist, delta_dist);
+
+                // perform DDA
+                while (hit == 0) {
+                    if (side_dist.x < side_dist.y) {
+                        side_dist.x += delta_dist.x;
+                        map_x += step_x;
+                        side = 0;
+                    }
+                    else {
+                        side_dist.y += delta_dist.y;
+                        map_y += step_y;
+                        side = 1;
+                    }
+                    // check if ray has hit a wall
+                    if (map[map_y][map_x] != EMPTY) {
+                        hit = 1;
+                    }
+                }
+
+                float ray_len = (side == 0) ? side_dist.x - delta_dist.x : side_dist.y - delta_dist.y;
+                Vector2 ray = Vector2Scale(ray_dir, ray_len);
+                // perpendicular distance from wall to camera plane
+                float perp_wall_dist = Vector2DotProduct(player_dir, ray);
+
+                // calculate height of line to render
+                int line_height = int(scr_height / perp_wall_dist);
+
+                // calculate the lowest and highest pixel to fill in current stripe
+                int draw_start = -line_height / 2 + scr_height / 2;
+                if (draw_start < 0) {
+                    draw_start = 0;
+                }
+                int draw_end = line_height / 2 + scr_height / 2;
+                if (draw_end >= scr_height) {
+                    draw_end = scr_height - 1;
+                }
+
+                // render walls
+                DrawLine(x, draw_start, x, draw_end, get_lighting(w_color[map[map_y][map_x]], side));
+                // render floor
+                DrawLine(x, draw_end, x, scr_height, get_lighting({ 100, 100, 100, 255 }, 1));
+                // render sky
+                DrawLine(x, 0, x, draw_start, SKY);
+            }
+
+            double delta_time = GetFrameTime();
+            float move_speed = delta_time * 5.f;
+            float rot_speed = delta_time * 2.f;
+            // movement
+            if (IsKeyDown(KEY_W)) {
+                Vector2 velocity = Vector2Scale(player_dir, move_speed);
+                if (map[int(player_pos.y)][int(player_pos.x + velocity.x)] == EMPTY) {
+                    player_pos.x += velocity.x;
+                }
+                if (map[int(player_pos.y + velocity.y)][int(player_pos.x)] == EMPTY) {
+                    player_pos.y += velocity.y;
+                }
+            }
+            if (IsKeyDown(KEY_S)) {
+                Vector2 velocity = Vector2Scale(player_dir, move_speed);
+                if (map[int(player_pos.y)][int(player_pos.x - velocity.x)] == EMPTY) {
+                    player_pos.x -= velocity.x;
+                }
+                if (map[int(player_pos.y - velocity.y)][int(player_pos.x)] == EMPTY) {
+                    player_pos.y -= velocity.y;
+                }
+            }
+
+            if (IsKeyDown(KEY_A)) {
+                player_dir = Vector2Rotate(player_dir, rot_speed);
+                camera_plane = Vector2Rotate(camera_plane, rot_speed);
+            }
+            if (IsKeyDown(KEY_D)) {
+                player_dir = Vector2Rotate(player_dir, -rot_speed);
+                camera_plane = Vector2Rotate(camera_plane, -rot_speed);
+            }
         }
 
         BeginDrawing();
-            ClearBackground(SKY);
+            ClearBackground(BLACK);
             rlImGuiBegin();
                 bool open = true;
                 ImGui::ShowDemoWindow(&open);
